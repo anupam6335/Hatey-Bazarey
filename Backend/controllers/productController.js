916dc2b9cdcf67f4dc202dbe76ccc71533e70182
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-19 19:24:40 +0530

    route for single and all product

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,7 +1,6 @@
 const Product = require('../models/product')
 
 // create new product  =>  /api/v1/product/new
-
 exports.newProduct = async (req, res, next) => {
 
 
@@ -13,9 +12,37 @@ exports.newProduct = async (req, res, next) => {
     })
 }
 
-exports.getProducts = (req, res, next) => {
+
+// Get all products   =>   /api/v1/products
+exports.getProducts = async (req, res, next) => {
+   
+    let products = await Product.find();
+
     res.status(200).json({
         success: true,
-        message: 'This route will show all the products in database. '
+        count: products.length,
+        products
     })
+}
+
+
+// Get single  product details  =>   /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+
 }
--- Backend/routes/product.js
+++ Backend/routes/product.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProducts, newProduct } = require("../controllers/productController");
+const { getProducts, newProduct, getSingleProduct } = require("../controllers/productController");
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
 
 router.route('/product/new').post(newProduct);
 module.exports = router;
