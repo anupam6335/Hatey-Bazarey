cc70bb72233f09d37ca2d62b4d7d5254cfdc5990
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-19 19:47:51 +0530

    update product route

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product')
 
-// create new product  =>  /api/v1/product/new
+// create new product  =>  /api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
 
 
@@ -43,6 +43,27 @@ exports.getSingleProduct = async (req, res, next) => {
         success: true,
         product
     })
+}
 
+// Update Product   =>   /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
 
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
 }
