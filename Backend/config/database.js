ebdff6b3206c0bfabadffff04e34aad477f5a277
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-20 14:26:47 +0530

    uncaught exception error handle

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+mongoose.set('strictQuery', false)
 
 const connectDatabase = () => {
   mongoose
--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,32 @@
-const app = require('./app');
-const connectDatabase = require('./config/database')
+const app = require("./app");
+const connectDatabase = require("./config/database");
 
-const dotenv  = require('dotenv');
+const dotenv = require("dotenv");
+
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
+
 // setting up config file
-dotenv.config({ path: 'Backend/config/config.env'})
-
+dotenv.config({ path: "Backend/config/config.env" });
 
 // Connecting to database
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
+// Handle Unhandled Promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err.message}`);
+  console.log("Shutting down the server due to Unhandled Promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
