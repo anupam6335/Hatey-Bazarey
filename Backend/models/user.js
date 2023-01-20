cae77739d061f1b2ad50b096a56785386aab75fd
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-21 00:20:03 +0530

    user model create

---
+++ Backend/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [30, "Your name cannot exceed 30 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [6, "Your password must be longer than 6 characters"],
+    select: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+module.exports = mongoose.model("User", userSchema);
