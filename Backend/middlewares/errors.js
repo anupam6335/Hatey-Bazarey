3f57a41c9f50004f7191ba048e53f811abccada4
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-20 18:04:12 +0530

    mongoose error handling

--- Backend/middlewares/errors.js
+++ Backend/middlewares/errors.js
@@ -17,6 +17,24 @@ module.exports = (err, req, res, next) => {
 
     error.message = err.message;
 
+    // Wrong Mongoose Object ID Error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid: ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling Mongoose Validation Error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling Mongoose duplicate key errors
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      error = new ErrorHandler(message, 400);
+    }
+    
     res.status(error.statusCode || 500).json({
       success: false,
       message: error.message || "Internal Server Error",
