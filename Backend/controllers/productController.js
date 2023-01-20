a7c12b5c67706ea4d40426664bef74374ad47d01
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-20 21:39:31 +0530

    filter products

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -15,7 +15,7 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 // Get all products   =>   /api/v1/products?keyword=apple
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
 
-  const apiFeatures = new APIFeatures(Product.find(), req.query).search()
+  const apiFeatures = new APIFeatures(Product.find(), req.query).search().filter()
 
   let products = await apiFeatures.query;
 
--- Backend/utils/apiFeatures.js
+++ Backend/utils/apiFeatures.js
@@ -1,21 +1,43 @@
 class APIFeatures {
-    constructor(query, queryStr) {
-        this.query = query;
-        this.queryStr = queryStr;
-    }
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
 
-    search() {
-        const keyword = this.queryStr.keyword ? {
-            name: {
-                $regex: this.queryStr.keyword,
-                $options: 'i'
-            }
-        } : {}
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
 
-        // console.log(keyword);
-        this.query = this.query.find({ ...keyword });
-        return this;
-    }
+    // console.log(keyword);
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    // console.log(queryCopy);
+
+    // Removing fields from the query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => delete queryCopy[el]);
+
+    // console.log(queryCopy);
+
+    // // Advance filter for price, ratings etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    // console.log(queryStr)
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 }
 
-module.exports = APIFeatures
+module.exports = APIFeatures;
