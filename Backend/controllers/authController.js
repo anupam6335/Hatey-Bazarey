f72ab7b3b6e018aada1a28559d2f1bd160603f27
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-22 07:24:30 +0530

    sending password by recovery email

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const sendToken = require("../utils/jwtToken");
+const sendEmail = require('../utils/sendEmail');
 
 // Register a user   => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -47,6 +48,50 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
   }
 
   sendToken(user, 200, res);
+})
+
+
+// Forgot Password   =>  /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler('User not found with this email', 404));
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    // Create reset password url
+    const resetUrl = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`
+
+    try {
+
+        await sendEmail({
+            email: user.email,
+            subject: 'Hatey Bazarey Password Recovery',
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`
+        })
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500))
+    }
+
 })
 
 
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser, logout } = require("../controllers/authController");
+const { registerUser, loginUser, forgotPassword, logout } = require("../controllers/authController");
 
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/password/forgot').post(forgotPassword)
+
 router.route('/logout').get(logout);
 
 
