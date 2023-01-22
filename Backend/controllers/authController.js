27f5efabf2ec5a0917b660e2934c2a2cad15da40
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-22 07:56:09 +0530

    reset new password

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -4,6 +4,8 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const sendToken = require("../utils/jwtToken");
 const sendEmail = require('../utils/sendEmail');
 
+const crypto = require('crypto');
+
 // Register a user   => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
 
@@ -66,7 +68,7 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     // Create reset password url
-    const resetUrl = `${req.protocol}://${req.get('host')}/password/reset/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
 
     const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`
 
@@ -94,6 +96,36 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 
 })
 
+// Reset Password   =>  /api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+
+    // Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    // Setup new password
+    user.password = req.body.password;
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+
+    sendToken(user, 200, res)
+
+})
 
 // Logout user   =>   /api/v1/logout
 exports.logout = catchAsyncErrors(async (req, res, next) => {
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser, forgotPassword, logout } = require("../controllers/authController");
+const { registerUser, loginUser, forgotPassword, resetPassword, logout } = require("../controllers/authController");
 
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
 router.route('/password/forgot').post(forgotPassword)
+router.route('/password/reset/:token').put(resetPassword)
 
 router.route('/logout').get(logout);
 
