87613c796964241c55e6b79be59f9db26e4a6e36
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-22 06:07:28 +0530

    adding user in product

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -4,6 +4,9 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const APIFeatures = require('../utils/apiFeatures')
 // create new product  =>  /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+
+  req.body.user = req.user.id;
+
   const product = await Product.create(req.body);
 
   res.status(201).json({
--- Backend/models/product.js
+++ Backend/models/product.js
@@ -84,6 +84,11 @@ const productSchema = new mongoose.Schema({
             }
         }
     ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
     createdAt: {
         type: Date,
         default: Date.now
