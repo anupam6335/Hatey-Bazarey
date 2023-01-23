f6c7d7e6d71eab26550d5e2c33d24f5a1bb9362a
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-24 03:44:19 +0530

    get all orders admin route

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -58,3 +58,21 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) => {
         orders
     })
 })
+
+
+// Get all orders - ADMIN  =>   /api/v1/admin/orders/
+exports.allOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find()
+
+    let totalAmount = 0;
+
+    orders.forEach(order => {
+        totalAmount += order.totalPrice
+    })
+
+    res.status(200).json({
+        success: true,
+        totalAmount,
+        orders
+    })
+})
--- Backend/routes/order.js
+++ Backend/routes/order.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const {
     newOrder,
     getSingleOrder,
-    myOrders
+    myOrders,
+    allOrders
 } = require('../controllers/orderController')
 
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth')
@@ -14,4 +15,5 @@ router.route('/order/new').post(isAuthenticatedUser, newOrder);
 router.route('/order/:id').get(isAuthenticatedUser, getSingleOrder);
 router.route('/orders/me').get(isAuthenticatedUser, myOrders);
 
+router.route('/admin/orders/').get(isAuthenticatedUser, authorizeRoles('admin'), allOrders);
 module.exports = router;
