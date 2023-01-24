3c4739e857feb22b786ffb997196124ec42a2ab7
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-25 01:21:25 +0530

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Header } from "./components/allComponents";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+   <h1 className="container">header page`</h1>
+  );
+};
+
+export default Header;
