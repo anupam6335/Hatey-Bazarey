c62321aa99d412d423322cea40f7a7b7c4e0a65f
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-27 14:58:07 +0530

    features product add

--- frontend/src/components/allComponents.js
+++ frontend/src/components/allComponents.js
@@ -4,3 +4,4 @@ export {default as Card} from './layout/Card/Card';
 export {default as User} from './layout/User/User';
 export {default as CartItems} from './layout/CartItems/CartItems';
 export {default as Featureproduct} from './layout/Featureproduct/Featureproduct';
+export {default as Product} from './layout/Featureproduct/Product';
--- frontend/src/components/layout/Featureproduct/Featureproduct.jsx
+++ frontend/src/components/layout/Featureproduct/Featureproduct.jsx
@@ -1,9 +1,10 @@
-import './Featureproduct.css';
-
+import "./Featureproduct.css";
+import { Product } from "../../allComponents";
+import { Link } from "react-router-dom";
 const Featureproduct = () => {
   return (
     <>
-      <div id='feature' className='section__p1'>
+      <div id="feature" className="section__p1">
         <div className="fe__box">
           <img src="/assets/features/f1.png" alt="" />
           <h6>Free Shipping</h6>
@@ -33,6 +34,23 @@ const Featureproduct = () => {
           <img src="/assets/features/f6.png" alt="" />
           <h6>F24/7 Support</h6>
         </div>
+      </div>
+
+      <div id="product1" className="section__p1">
+        <h2>Featured Products</h2>
+        <p>Summer Collection New Modern Design</p>
+        <Link to="/products" className="view__product">
+          <div className="pro__container">
+            <Product imgId="f1" imgName='Cartoon Astronaut T-Shirts' brandName='addidas' price='58'/>
+            <Product imgId="f2" imgName='Cartoon Astronaut T-Shirts' brandName='zara' price='58'/>
+            <Product imgId="f3" imgName='Cartoon Astronaut T-Shirts' brandName='fusion' price='58'/>
+            <Product imgId="f4" imgName='Cartoon Astronaut T-Shirts' brandName='addidas' price='58'/>
+            <Product imgId="f5" imgName='Cartoon Astronaut T-Shirts' brandName='zoya' price='58'/>
+            <Product imgId="f6" imgName='Cartoon Astronaut T-Shirts' brandName='addidas' price='58'/>
+            <Product imgId="f7" imgName='Cartoon Astronaut T-Shirts' brandName='addidas' price='58'/>
+            <Product imgId="f8" imgName='Cartoon Astronaut T-Shirts' brandName='addidas' price='58'/>
+          </div>
+        </Link>
       </div>
     </>
   );
