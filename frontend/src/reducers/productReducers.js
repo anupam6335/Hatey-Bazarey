c4faecda5513fbdb4a50d5de323daea46acd8127
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-27 20:37:10 +0530

    create all product reducer

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,39 @@
+import {
+    ALL_PRODUCTS_REQUEST,
+    ALL_PRODUCTS_SUCCESS,
+    ALL_PRODUCTS_FAIL,
+    CLEAR_ERRORS
+
+} from '../constants/productConstants'
+
+export const productsReducer = (state = { products: [] }, action) => {
+    switch (action.type) {
+        case ALL_PRODUCTS_REQUEST:
+            return {
+                loading: true,
+                products: []
+            }
+
+        case ALL_PRODUCTS_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload.products,
+                productsCount: action.payload.productsCount,
+            }
+
+        case ALL_PRODUCTS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state;
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productsReducer } from './reducers/productReducers'
 
 const reducer = combineReducers({});
 
-let initialState = {};
+let initialState = {
+    products: productsReducer
+};
 
 const middlware = [thunk];
 const store = createStore(
