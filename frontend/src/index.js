dcdd3a7bc24f2abafa38c6489ae336979eb2c79f
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-27 20:12:15 +0530

    create store for redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
---
+++ frontend/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+let initialState = {};
+
+const middlware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middlware))
+);
+
+export default store;
