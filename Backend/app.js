408ade45f35cf38dd35225805e10c97beb82b510
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-02-01 00:23:51 +0530

    shift app.js to server.js cloudinary code

--- Backend/app.js
+++ Backend/app.js
@@ -3,19 +3,14 @@ const app = express();
 
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const cloudinary = require('cloudinary')
+const fileUpload = require('express-fileupload')
 const errorMiddleware = require('./middlewares/errors')
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(fileUpload());
 
-// Setting up cloudinary configuration
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
 
 // import all routes
 const products = require('./routes/product');
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
+const cloudinary = require('cloudinary')
 
 // Handle Uncaught exceptions
 process.on("uncaughtException", (err) => {
@@ -12,6 +13,13 @@ process.on("uncaughtException", (err) => {
 
 // setting up config file
 dotenv.config({ path: "Backend/config/config.env" });
+
+// Setting up cloudinary configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 // Connecting to database
 connectDatabase();
