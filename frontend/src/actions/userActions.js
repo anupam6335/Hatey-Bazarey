1b39f4123986c07c9c4798addd78a7958f524705
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-01-31 23:52:23 +0530

    Register User Reducer & Action create

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,9 @@ import {
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
     CLEAR_ERRORS
 } from '../constants/userConstants'
 
@@ -33,6 +36,32 @@ export const login = (email, password) => async (dispatch) => {
     }
 }
 
+// Register user
+export const register = (userData) => async (dispatch) => {
+    try {
+
+        dispatch({ type: REGISTER_USER_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const { data } = await axios.post('/api/v1/register', userData, config)
+
+        dispatch({
+            type: REGISTER_USER_SUCCESS,
+            payload: data.user
+        })
+
+    } catch (error) {
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
 
 
 // Clear Errors
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,18 +2,23 @@ import {
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
   CLEAR_ERRORS,
 } from "../constants/userConstants";
 
 export const authReducer = (state = { user: {} }, action) => {
   switch (action.type) {
     case LOGIN_REQUEST:
+    case REGISTER_USER_REQUEST:
       return {
         loading: true,
         isAuthenticated: false,
       };
 
     case LOGIN_SUCCESS:
+    case REGISTER_USER_SUCCESS:
       return {
         ...state,
         loading: false,
@@ -22,6 +27,7 @@ export const authReducer = (state = { user: {} }, action) => {
       };
 
     case LOGIN_FAIL:
+    case REGISTER_USER_FAIL:
       return {
         ...state,
         loading: false,
