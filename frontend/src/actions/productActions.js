550b37c0b7becbc3c6d6c21c608dbde48e994bae
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2023-02-07 18:01:49 +0530

    new review action reducer create

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -7,6 +7,10 @@ import {
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL,
+    NEW_REVIEW_REQUEST,
+    NEW_REVIEW_SUCCESS,
+    NEW_REVIEW_RESET,
+    NEW_REVIEW_FAIL,
     CLEAR_ERRORS
 
 } from '../constants/productConstants'
@@ -50,6 +54,32 @@ export const getProductDetails = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_DETAILS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+export const newReview = (reviewData) => async (dispatch) => {
+    try {
+
+        dispatch({ type: NEW_REVIEW_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.put(`/api/v1/review`, reviewData, config)
+
+        dispatch({
+            type: NEW_REVIEW_SUCCESS,
+            payload: data.success
+        })
+
+    } catch (error) {
+        dispatch({
+            type: NEW_REVIEW_FAIL,
             payload: error.response.data.message
         })
     }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,6 +5,10 @@ import {
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL,
+    NEW_REVIEW_REQUEST,
+    NEW_REVIEW_SUCCESS,
+    NEW_REVIEW_RESET,
+    NEW_REVIEW_FAIL,
     CLEAR_ERRORS
 } from '../constants/productConstants'
 
@@ -60,6 +64,46 @@ export const productDetailsReducer = (state = { product: {} }, action) => {
             return {
                 ...state,
                 error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state
+    }
+}
+
+
+
+export const newReviewReducer = (state = {}, action) => {
+    switch (action.type) {
+
+        case NEW_REVIEW_REQUEST:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case NEW_REVIEW_SUCCESS:
+            return {
+                loading: false,
+                success: action.payload
+            }
+
+        case NEW_REVIEW_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
+
+        case NEW_REVIEW_RESET:
+            return {
+                ...state,
+                success: false
             }
 
         case CLEAR_ERRORS:
